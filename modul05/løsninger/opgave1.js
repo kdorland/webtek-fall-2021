8bf8c9f52b64a9061435a05c4384472f1b6aae03
Kristian Dorland <kristian@dorland.dk>
2021-10-04 10:39:09 +0200

    Løsning på opgave 1, modul 5

---
+++ modul05/løsninger/opgave1.js
@@ -0,0 +1,11 @@
+function sumTalrække(a, b) {
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+console.log(sumTalrække(0,10));
+console.log(sumTalrække(1,3)); // Skal udskrive 6
+console.log(sumTalrække(2,5)); // Skal udskrive 14
